e477f99be983517732c1004fa2108511797b405f
alexb5dh <alexb.5dh@gmail.com>
2016-06-18 18:24:36 +0300

    Implement both Greedy algorithms for Algorithms2

--- Algorithms2/greedy.py
+++ Algorithms2/greedy.py
@@ -4,8 +4,11 @@ from collections import namedtuple
 
 Job = namedtuple('Job', 'weight length')
 
-def shedule(jobs):
+def shedule_by_difference(jobs):
     return sorted(jobs, key = lambda job: (job.weight - job.length, job.weight), reverse = True)
+
+def shedule_by_ratio(jobs):
+    return sorted(jobs, key = lambda job: (float(job.weight) / job.length), reverse = True)
 
 def objective(schedule):
     res, c = 0, 0
@@ -18,9 +21,8 @@ jobs = []
 with open(argv[1], 'r') as lines:
     n = int(next(lines))
     for i in xrange(n):
-        vals = map(int, next(lines).split())
-        jobs.append(Job(weight = vals[0], length = vals[1]))
+        jobs.append(Job(*map(int, next(lines).split())))
 
-scheduled = shedule(jobs)
-res = objective(scheduled)
-print (res)
+by_diff = shedule_by_difference(jobs)
+by_ratio = shedule_by_ratio(jobs)
+print ('By difference:\t{0}\nBy ratio:\t{1}'.format(objective(by_diff), objective(by_ratio)))
